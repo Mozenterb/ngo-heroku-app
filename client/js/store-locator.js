81edd339cee2eec8e7bb9ee7070e5b4f6b41ace3
elizabeth-martin <elizabeth.martin@salesforce.com>
2017-07-18 21:19:57 -0400

    add

--- client/js/store-locator.js
+++ client/js/store-locator.js
@@ -33,12 +33,9 @@ angular.module('nibs.store-locator', [])
             currentPosMarker,
 
             icon = L.icon({
-                iconUrl: 'img/leaf-green.png',
-                shadowUrl: 'img/leaf-shadow.png',
-                iconSize:     [38, 95], // size of the icon
-                shadowSize:   [50, 64], // size of the shadow
-                iconAnchor:   [22, 94], // point of the icon which will correspond to marker's location
-                shadowAnchor: [4, 62],  // the same for the shadow
+                iconUrl: 'img/mapicon.png',
+                iconSize:     [24, 24], // size of the icon
+                iconAnchor:   [12, 0], // point of the icon which will correspond to marker's location
                 popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
             });
 
